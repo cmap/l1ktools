8ddbb7ba23711abdfe82180a613ebc4b99ef02a0
Oana Enache <oamenache@gmail.com>
2016-10-18 12:55:28 -0400

    added commenting/documentation

--- python/broadinstitute/cmap/io/GCToo/parse_gctoox.py
+++ python/broadinstitute/cmap/io/GCToo/parse_gctoox.py
@@ -1,6 +1,30 @@
+"""
+Reads in a .gctx file as a gctoo object. 
+
+The main method is parse; subsequent methods are helpers that read/format different 
+components of the underlying HDF5 file.
+
+Note. Only supports v1.0 gctx files. 
+
+Structure of a complete (minimal) GCTX:
+
+	/version -> required. Version of the file; currently we only support 1.0
+	/src -> required. Input source of file 
+	/0
+		/DATA 
+			/0
+				/matrix -> required. Data matrix of expression values. 
+		/META 
+			/ROW 
+				/id -> required. Unique row identifiers. 
+				/... -> optional. Other row metadata fields 
+			/COL 
+				/id -> required. Unique col identifiers. 
+				/... -> optional. Other col metadata fields 			
+
+"""
 import logging
 import setup_GCToo_logger as setup_logger
-# import sys
 import os 
 import numpy as np 
 import pandas as pd 
@@ -13,7 +37,7 @@ __email__ = "oana@broadinstitute.org"
 #instantiate logger
 logger = logging.getLogger(setup_logger.LOGGER_NAME)
 # when not in debug mode, probably best to set verbose=False
-setup_logger.setup(verbose = True)
+setup_logger.setup(verbose = False)
 
 version_node = "version"
 rid_node = "/0/META/ROW/id"
@@ -39,7 +63,7 @@ def parse(gctx_file_path, convert_neg_666=True, rid=None, cid=None):
 	Output:
 		- myGCToo 
 
-	Note: why is convert_neg_666 even a thing? 
+	Note: why does convert_neg_666 exist? 
 		- In CMap--for somewhat obscure historical reasons--we use "-666" as our null value 
 		for metadata. However (so that users can take full advantage of pandas' methods, 
 		including those for filtering nan's etc) we provide the option of converting these 
@@ -50,32 +74,25 @@ def parse(gctx_file_path, convert_neg_666=True, rid=None, cid=None):
 	gctx_file = h5py.File(full_path, "r", driver = "core")
 
 	# get version
-	# Note: this should be "GCTX1.0"
 	my_version = gctx_file.attrs[version_node][0]
 
 	# get dsets corresponding to rids, cids 
 	rid_dset = gctx_file[rid_node]
 	cid_dset = gctx_file[cid_node]
-	# store appropriate id information in dict for O(1) lookup
-	# Note: if slicing in 
+	# store commonly accessed id-related information in dict for O(1) lookup
 	id_info = make_id_info_dict(rid_dset, cid_dset, rid, cid)
 
-	# get data dset
+	# Reads data (or specified slice thereof) from input GCTX to pandas DataFrame
 	data_dset = gctx_file[data_node]
-	# write data (or specified slice thereof) to pandas DataFrame
 	data_df = parse_data_df(data_dset, id_info)
 
-	# get row metadata
+	# Reads row metadata (or specified slice thereof) from input GCTX to pandas DataFrame
 	row_group = gctx_file[row_meta_group_node]
 	row_meta_df = make_meta_df("rids", row_group, id_info, convert_neg_666)
-	logger.debug("row_meta_df index: {}".format(row_meta_df.index))
-	logger.debug("row_meta_df columns: {}".format(row_meta_df.columns))
 
-	# get col metadata
+	# Reads col metadata (or specified slice thereof) from input GCTX to pandas DataFrame
 	col_group = gctx_file[col_meta_group_node]
 	col_meta_df = make_meta_df("cids", col_group, id_info, convert_neg_666)
-	logger.debug("col_meta_df index: {}".format(col_meta_df.index))
-	logger.debug("col_meta_df columns: {}".format(col_meta_df.columns))
 
 	# close file 
 	gctx_file.close()
@@ -88,18 +105,28 @@ def parse(gctx_file_path, convert_neg_666=True, rid=None, cid=None):
 
 def make_id_info_dict(rid_dset, cid_dset, rid, cid):
 	"""
-	TODO
+	Generactes a dict to commonly accessed id-related information for input file.
+
+	Input:
+		- rid_dset (HDF5 Dataset): Array of rid values. 
+		- cid_dset (HDF5 Dataset): Array of cid values. 
+		- rid (list): Either a number of rids to subset from full list, or None. 
+		- cid (list): Either a number of cids to subset from full list, or None. 
+
+	Output: 
+		- id_dict (dict)
 	"""
-	# for looking up id information 
-	# (full & subsets, as applicable)
 	id_dict = {}
+
 	# read in full id values 
 	get_all_id_values(rid_dset, cid_dset, id_dict)
+
 	# to store slice lengths (if applicable)
 	# Note: Currently h5py throws an error if you try hyperslab selection along
 	#  		more than one dimension at once, so we hyperslab first by the longer slice length
 	#		(in cases where both dimensions are sliced)
 	id_dict["slice_lengths"] = {}
+
 	# set subsetted id values to whatever's applicable
 	# if taking slices w/hyperslabs, need to obtain an ordered list of indexes
 	if rid != None:
@@ -108,25 +135,36 @@ def make_id_info_dict(rid_dset, cid_dset, rid, cid):
 	if cid != None:
 		get_ordered_slice_indexes("cids", id_dict, cid)
 		id_dict["slice_lengths"]["cids"] = len(cid)
-	return id_dict
 
+	return id_dict
 
 def get_all_id_values(rid_dset, cid_dset, id_dict):
 	"""
-	TODO
+	Helper to make_id_info_dict(). 
+	Populates and properly formats rids and cids from input file.
+
+	Input:
+		- rid_dset (HDF5 Dataset): Array of rid values. 
+		- cid_dset (HDF5 Dataset): Array of cid values. 
+		- id_dict (dict): Dictionary of commonly accessed id-related information for input file.
+
+	Output:
+		None
 	"""
 	# make empty numpy arrays of proper dims to populate
 	# Note: ids are currently truncated at > 50 chars
-	# TODO: Q: What's the limit for id lengths? 
 	rid_array = np.empty(rid_dset.shape, dtype = "S50")
 	cid_array = np.empty(cid_dset.shape, dtype = "S50")
+
 	# set numpy arrays to respective id values 
 	rid_dset.read_direct(rid_array)
 	cid_dset.read_direct(cid_array)
+
 	# convert to pandas DataFrames
 	# this structure enables us to easily get a list of slice value indexes
 	rid_df = pd.DataFrame(range(0, rid_array.shape[0]), index = rid_array).transpose()
 	cid_df = pd.DataFrame(range(0, cid_array.shape[0]), index = cid_array).transpose()
+	
 	# add full values to id info dict
 	id_dict["rids"] = {}
 	id_dict["rids"]["full_id_list"] = rid_df
@@ -135,15 +173,30 @@ def get_all_id_values(rid_dset, cid_dset, id_dict):
 
 def get_ordered_slice_indexes(dim_id_key, id_dict, slice_id_list):
 	"""
-	TODO: Explain why this is necessary for hyperslab slicing w/h5py
+	Helper to make_id_info_dict(). 
+	Determines numerical indexes and orders them appropriately for compatibility
+	with fancy indexing requirements for h5py v.2.5.
+
+	(Oct. 2016) See: http://docs.h5py.org/en/latest/high/dataset.html#fancy-indexing
+
+	Input:
+		- dim_id_key (str, either "rids" or "cids"): dimension on which to perform operations. 
+		- id_dict (dict): Dictionary of commonly accessed id-related information for input file.
+		- slice_id_list (list): list of id values to slice out  
+
+	Output:
+		None 
 	"""
 	# get unsorted index values of slice_id_list 
 	unordered_slice_df = id_dict[dim_id_key]["full_id_list"].loc[:, slice_id_list]
+	
 	# gets indexes that would properly sort slice_id_list values, 
 	# and orders elements of slice_id_list accordingly
 	ordered_columns = unordered_slice_df.columns[unordered_slice_df.ix[unordered_slice_df.last_valid_index()].argsort()]	
+	
 	# order index values corresponding to slice_id_list in ascending numerical order
 	ordered_slice_df = unordered_slice_df[ordered_columns]
+	
 	# set 
 	id_dict[dim_id_key]["slice_indexes"] = ordered_slice_df[ordered_columns].values.tolist()[0]
 	id_dict[dim_id_key]["slice_values"] = list(ordered_columns)
@@ -151,7 +204,14 @@ def get_ordered_slice_indexes(dim_id_key, id_dict, slice_id_list):
 
 def parse_data_df(data_dset, id_dict):
 	"""
-	TODO
+	Reads data (or specified slice thereof) from input GCTX to pandas DataFrame. 
+
+	Input:
+		- data_dset (HDF5 Dataset): Dataset of matrix of expression values 
+		- id_dict (dict): Dictionary of commonly accessed id-related information for input file.
+
+	Output:
+		- data_df (pandas DataFrame): data frame of expression values 
 
 	NOTE: Data_DF is stored transposed from final parsed form;
 		so, the indexing of slicing is the opposite of what would normally
@@ -160,6 +220,7 @@ def parse_data_df(data_dset, id_dict):
 	# for setting proper index/columns of data df 
 	rids = id_dict["rids"]["full_id_list"].columns
 	cids = id_dict["cids"]["full_id_list"].columns 
+	
 	# if applicable, determine how to slice hdf5 file
 	(slice_both, first_dim) = set_slice_order(id_dict) 
 	if slice_both: # slice both columns 
@@ -183,35 +244,56 @@ def parse_data_df(data_dset, id_dict):
 		data_array = np.empty(data_dset.shape, dtype = np.float64) 
 		data_dset.read_direct(data_array)
 
-	logger.debug("rids: {}".format(rids))
-	logger.debug("cids: {}".format(cids))
 	data_df = pd.DataFrame(data_array.transpose(), index = rids, columns = cids)
 	data_df.index.name = "rid"
 	data_df.columns.name = "cid"
-	logger.debug("parsed data_df: {}".format(data_df))
+
 	return data_df
 
 def set_slice_order(id_dict):
 	"""
-	TODO
+	Helper to parse_data_df(). 
+	For h5py v.2.5, we can only slice on one dimension at a time; so, the larger dimension is 
+	sliced first. 
+
+	TODO (oana): This is not necessarily optimal; fix to instead slice by largest area 
+
+	Input: 
+		- id_dict (dict): Dictionary of commonly accessed id-related information for input file.
+
+	Output:
+		- slice_both_dims (bool): whether to slice both dims or not 
+		- first_slice_dim (str): first dimension to slice on 
+
 	"""
 	# (for current h5py version) can only take hyperslab on one dimension
 	# ... so we choose the larger one 
 	slice_both_dims = False 
 	first_slice_dim = None 
+
 	if len(id_dict["slice_lengths"]) == 2: 
-		first_slice_dim = max(id_dict['slice_lengths'].iterkeys(), key=(lambda key: id_dict["slice_lengths"][key]))
+		first_slice_dim = max(id_dict['slice_lengths'].iterkeys(), 
+			key=(lambda key: id_dict["slice_lengths"][key]))
 		slice_both_dims = True
 	elif len(id_dict["slice_lengths"]) == 1:
 		first_slice_dim = id_dict["slice_lengths"].keys()[0]
+
 	return slice_both_dims, first_slice_dim
 
 def make_meta_df(dim_id_key, dset, id_dict, convert_neg_666):
 	"""
+	Reads metadata (or specified slice thereof) from input GCTX to pandas DataFrame
+
+	Input:
+		- dim_id_key (str; either "rids" or "cids"): dimension of ids to get metadata for 
+		- dset (HDF5 Group): Group from which to parse metadata values 
+		- id_dict (dict): Dictionary of commonly accessed id-related information for input file.
+		- convert_neg_666 (bool): Whether to convert -666 values to numpy.Nan
+
+	Output:
+		- meta_df (pandas DataFrame)
 	"""
 	(row_indexes, df_index_vals) = set_meta_index_to_use(dim_id_key, id_dict)
-	# logger.debug("row_indexes: {}".format(row_indexes))
-	# logger.debug("{} df index vals: {}".format(dim_id_key, df_index_vals))
 
 	if len(dset.keys()) > 1: # aka if there is metadata besides ids
 		meta_df = populate_meta_array(dset, row_indexes)
@@ -219,7 +301,6 @@ def make_meta_df(dim_id_key, dset, id_dict, convert_neg_666):
 		# column values shouldn't include "id"
 		df_column_vals = list(dset.keys()[:])
 		df_column_vals.remove("id")
-		# logger.debug("df columns vals: {}".format(df_column_vals))
 
 		meta_df.index = df_index_vals
 		meta_df.columns = df_column_vals
@@ -241,6 +322,18 @@ def make_meta_df(dim_id_key, dset, id_dict, convert_neg_666):
 	return meta_df
 
 def set_meta_index_to_use(dim_id_key, id_dict):
+	"""
+	Helper to make_meta_df. 
+	Sets indexes/columns of metadata df appropriately.
+
+	Input:
+		- dim_id_key (str; either "rids" or "cids"): dimension of ids to get metadata for
+		- id_dict (dict): Dictionary of commonly accessed id-related information for input file.
+
+	Output:
+		- row_indexes (list): numerical indexes to keep for metadata df 
+		- df_index_vals (list): values of index to keep for metadata df 
+	"""
 	if len(id_dict[dim_id_key]) == 3: # yes slice
 		row_indexes = id_dict[dim_id_key]["slice_indexes"]
 		df_index_vals = id_dict[dim_id_key]["slice_values"]
@@ -250,15 +343,22 @@ def set_meta_index_to_use(dim_id_key, id_dict):
 	return row_indexes, df_index_vals	
 
 def populate_meta_array(meta_group, row_indexes):
+	"""
+	Helper to make_meta_df().
+
+	Input:
+		- meta_group (HDF5 group): Group from which to read metadata values 
+		- row_indexes (list): indexes of metadata fields to keep 
+
+	Output:
+		- meta_value_df (pandas DataFrame)
+	"""
 	meta_values = {}
 	for k in meta_group.keys():
 		if k != "id":
 			with meta_group[k].astype("S50"):
 				curr_meta = list(meta_group[k][row_indexes])
-				# logger.debug("curr_meta values: {}".format(curr_meta))
-				# logger.debug("curr_meta as list: {}".format(list(curr_meta)))
 				meta_values[k] = [str(elem).strip() for elem in curr_meta]
-	# logger.debug("meta values before concat: {}".format(meta_values))
 	meta_value_df = pd.DataFrame.from_dict(meta_values)
 	return meta_value_df
 
