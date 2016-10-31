284c5cd95f1704ad588d9019198cb11f33fe9de5
Dave Lahr <dlahr@broadinstitute.org>
2016-10-31 12:52:26 -0400

    clue_api_client/cell_queries:  minor variable rename

--- python/broadinstitute_cmap/clue_api_client/cell_queries.py
+++ python/broadinstitute_cmap/clue_api_client/cell_queries.py
@@ -8,7 +8,7 @@ __email__ = "dlahr@broadinstitute.org"
 logger = logging.getLogger(setup_logger.LOGGER_NAME)
 
 
-def is_cell_line_in_api(clue_api_client, cell_id):
-        query_result = clue_api_client.run_count_query("cells", {"cell_id":cell_id})
+def is_cell_line_in_api(my_clue_api_client, cell_id):
+        query_result = my_clue_api_client.run_count_query("cells", {"cell_id":cell_id})
         logger.debug("query_result:  {}".format(query_result))
         return query_result["count"] == 1
