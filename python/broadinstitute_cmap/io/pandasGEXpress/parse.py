161ead087942ba7ca9386007332200b04f2ea83f
lev <lev@broadinstitute.org>
2017-01-12 19:17:27 -0500

    parse.py: added make_multiindex arg to this function too

--- python/broadinstitute_cmap/io/pandasGEXpress/parse.py
+++ python/broadinstitute_cmap/io/pandasGEXpress/parse.py
@@ -18,7 +18,8 @@ __email__ = "oana@broadinstitute.org"
 # instantiate logger
 logger = logging.getLogger(setup_logger.LOGGER_NAME)
 
-def parse(file_path, convert_neg_666=True, rid=None, cid=None, nan_values=None, meta_only=None):
+def parse(file_path, convert_neg_666=True, rid=None, cid=None,
+		  meta_only=None, make_multiindex=False):
 	""" 
 	Identifies whether file_path corresponds to a .gct or .gctx file and calls the
 	correct corresponding parse method.
@@ -31,8 +32,9 @@ def parse(file_path, convert_neg_666=True, rid=None, cid=None, nan_values=None, 
 		- convert_neg_666 (bool): whether to convert -666 values to numpy.nan or not 
 			(see Note below for more details on this). Default = False.
 		- rid (list of strings): list of row ids to specifically keep from gctx. Default=None. 
-		- cid (list of strings): list of col ids to specifically keep from gctx. Default=None. 
-
+		- cid (list of strings): list of col ids to specifically keep from gctx. Default=None.
+		- make_multiindex (bool): whether to create a multi-index df combining
+            the 3 component dfs
 
 	Output:
 		- myGCToo (GCToo)
@@ -44,9 +46,9 @@ def parse(file_path, convert_neg_666=True, rid=None, cid=None, nan_values=None, 
 		into numpy.NaN values, the pandas default. 
 	""" 
 	if file_path.endswith(".gct"):
-		curr = parse_gct.parse(file_path, convert_neg_666, rid, cid)
+		curr = parse_gct.parse(file_path, convert_neg_666, rid, cid, make_multiindex)
 	elif file_path.endswith(".gctx"):
-		curr = parse_gctx.parse(file_path, convert_neg_666, rid, cid, meta_only)
+		curr = parse_gctx.parse(file_path, convert_neg_666, rid, cid, meta_only, make_multiindex)
 	else:
 		logger.error("File to parse must be .gct or .gctx!")
 	return curr 
