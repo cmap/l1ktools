785b6faa5a7712e9f13c0ce5dbd4d3cdeadd90bb
Jacob Asiedu <jasiedu@broadinstitute.org>
2017-05-17 20:14:48 -0400

    Deprecating master branch

--- java/runGCTXReaderExampleMacOsX.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Running on 64 bit MacOsX"
-java -Djava.library.path=lib/native/macosx/64 -cp $CLASSPATH ReadGctxExample
--- python/setup.py
+++
@@ -1,6 +0,0 @@
-import setuptools
-
-setuptools.setup(
-	name="broadinstitute_cmap",
-	packages=["broadinstitute_cmap"]
-)
